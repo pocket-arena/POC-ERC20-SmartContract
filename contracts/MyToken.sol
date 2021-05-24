d3b5dbf7e4a2c3c5a05dc560fc20368aeac2bb58
pocket-arena <82522405+pocket-arena@users.noreply.github.com>
2021-05-24 11:01:13 +0900

    Update MyToken.sol
    * Updated as security audit

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,83 +1,89 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-solidity/contracts/utils//math/SafeMath.sol";
 contract MyToken is ERC20{
- uint256 private _schedule_term;
- uint256 private _mint_term;
- address _owner;
- uint256 private _initSupplyPOC;
+ uint256 constant INIT_SUPPLY_POC = 1000000000;
+ uint8 constant SECONDS_PER_BLOCK = 15;
+  
+ uint256 constant SCHEDULE_TERM = (60 * 60 * 24 * 30) / SECONDS_PER_BLOCK;  // during 30 days
+ uint256 constant MINT_TERM = (60 * 60 * 24 * 730) / SECONDS_PER_BLOCK;  // during 730 days
+ address constant _approver1 = 0x2C76A35B071b9299b538c93686903c8Ab9F06e5e;
+ address constant _approver2 = 0x65d6D8353566Be8866a03B41d21173C647DBa0dD;
+ 
+ //uint256 constant SCHEDULE_TERM = 30 / SECONDS_PER_BLOCK;  // during 30 seconds
+ //uint256 constant MINT_TERM = 730 / SECONDS_PER_BLOCK;  // during 730 seconds
+ //address constant _approver1 = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
+ //address constant _approver2 = 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB;
+ 
+ address _owner; 
+ 
  uint256 private _addedSupplyToken;
  uint256 private _listingDate;
+ uint256 private _burnApproved1 = 0;
+ uint256 private _burnApproved2 = 0;
+ uint256 private _mintApproved1 = 0;
+ uint256 private _mintApproved2 = 0;
+ 
  struct Schedule{
-   uint256 day;
-   uint256 POC;
+  uint256 day;
+  uint256 POC;
  }
  Schedule[] private schedule;
  
- constructor(uint listing_days) ERC20("PocketArena", "POC"){
-  _schedule_term = 30 days;
-  _mint_term = 730 days;
-  //_schedule_term = 30 seconds;  
-  //_mint_term = 730 seconds;
-
-  _listingDate = block.timestamp + listing_days;
+ constructor() ERC20("PocketArena", "POC"){
+  _listingDate = block.number;
   _owner = msg.sender;
-  _initSupplyPOC = 1000000000;
-  _mint(_owner, SafeMath.mul(_initSupplyPOC, (10 ** uint256(decimals()))));
+  _mint(_owner, (INIT_SUPPLY_POC * (10 ** uint256(decimals()))));
   _addedSupplyToken = 0;  
   
   schedule.push(Schedule(_listingDate, 501666667));  
-  schedule.push(Schedule(SafeMath.add(_listingDate, _schedule_term), 503333334));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 2)), 505000001));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 3)), 506666668));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 4)), 508333335));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 5)), 510000002));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 6)), 526666669));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 7)), 528333336));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 8)), 552500003));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 9)), 554166670));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 10)), 578333337));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 11)), 580000004));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 12)), 754166671));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 13)), 755833338));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 14)), 780000005));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 15)), 781666672));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 16)), 805833339));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 17)), 807500006));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 18)), 831666673));  
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 19)), 1666667));
-  schedule.push(Schedule(SafeMath.add(_listingDate, SafeMath.mul(_schedule_term, 119)), _initSupplyPOC));
+  schedule.push(Schedule((_listingDate + SCHEDULE_TERM), 503333334));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 2)), 505000001));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 3)), 506666668));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 4)), 508333335));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 5)), 510000002));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 6)), 526666669));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 7)), 528333336));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 8)), 552500003));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 9)), 554166670));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 10)), 578333337));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 11)), 580000004));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 12)), 754166671));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 13)), 755833338));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 14)), 780000005));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 15)), 781666672));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 16)), 805833339));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 17)), 807500006));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 18)), 831666673));  
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 19)), 1666667));
+  schedule.push(Schedule((_listingDate + (SCHEDULE_TERM * 119)), INIT_SUPPLY_POC));
  }
- function listingDateGet() public view virtual returns (uint256) {
-  return _listingDate;
- }
- function scheduleGet(uint16 round) public virtual view returns (Schedule memory) {
+ function scheduleGet(uint16 round) external view returns (Schedule memory) {
    return schedule[round];
  }
  function lockedPOC(uint256 currentDate) public view returns (uint256) {
-  if (schedule[SafeMath.sub(schedule.length, 1)].day <= currentDate) {
-   //return SafeMath.sub(_initSupplyPOC, schedule[SafeMath.sub(schedule.length, 1)].POC);
+  if (schedule[(schedule.length - 1)].day <= currentDate) {
+   //return (INIT_SUPPLY_POC - schedule[(schedule.length - 1)].POC);
    return 0;
   }
-  else if (schedule[SafeMath.sub(schedule.length, 2)].day <= currentDate) { 
-   uint dateDiff = SafeMath.div(SafeMath.sub(currentDate, schedule[SafeMath.sub(schedule.length, 2)].day), _schedule_term);
-   uint256 newUnlockPOC = SafeMath.mul(schedule[SafeMath.sub(schedule.length, 2)].POC, SafeMath.add(dateDiff, 1));
-   return SafeMath.sub(_initSupplyPOC, SafeMath.add(schedule[SafeMath.sub(schedule.length, 3)].POC, newUnlockPOC));
+  else if (schedule[(schedule.length - 2)].day <= currentDate) { 
+   uint dateDiff = ((currentDate - schedule[(schedule.length - 2)].day) / SCHEDULE_TERM);
+   uint256 newUnlockPOC = (schedule[(schedule.length - 2)].POC * (dateDiff + 1));
+   return (INIT_SUPPLY_POC - (schedule[(schedule.length - 3)].POC + newUnlockPOC));
   }
   else {
-   for (uint i=SafeMath.sub(schedule.length, 1); i>0; i--) {
+   for (uint i=(schedule.length - 1); i>0; i--) {
     if (schedule[i-1].day <= currentDate) {
-     return SafeMath.sub(_initSupplyPOC, schedule[i-1].POC);
+     return (INIT_SUPPLY_POC - schedule[i-1].POC);
     }
    }
-   return _initSupplyPOC;
+   return INIT_SUPPLY_POC;
   }
  }
  function transferable() public view returns (uint256) {
-   uint256 locked = SafeMath.mul(lockedPOC(block.timestamp), (10 ** uint256(decimals())));
+   uint256 locked = (lockedPOC(block.number) * (10 ** uint256(decimals())));
    if (balanceOf(_owner) > locked) {
-	   return SafeMath.sub(balanceOf(_owner), locked);
+	   return (balanceOf(_owner) - locked);
    }
    else {
       return 0;
@@ -85,7 +91,11 @@ contract MyToken is ERC20{
  }
 
  modifier listingDT() {
-  require(_listingDate <= block.timestamp, "listing is not yet");
+  require(_listingDate <= block.number, "listing is not yet");
+  _;
+ }
+ modifier onlyApprover() {
+  require((msg.sender == _approver1 || msg.sender == _approver2), "only approver is possible");
   _;
  }
  modifier onlyOwner() {
@@ -101,17 +111,79 @@ contract MyToken is ERC20{
    _;
   }
  }
- function burn(uint256 burnToken) listingDT onlyOwner public returns (bool) {
+ 
+ function burn_approve_up(uint256 approveToken) onlyApprover external returns (bool) {
+  if (msg.sender == _approver1) {
+      _burnApproved1 = approveToken;
+  }
+  else if (msg.sender == _approver2) {
+      _burnApproved2 = approveToken;
+  }
+  return true;
+ }
+ function burn_approve_down() onlyApprover external returns (bool) {
+  if (msg.sender == _approver1) {
+      _burnApproved1 = 0;
+  }
+  else if (msg.sender == _approver2) {
+      _burnApproved2 = 0;
+  }
+  return true;
+ }
+ function burn(uint256 burnToken) listingDT onlyOwner external returns (bool) {
    require(_addedSupplyToken >= burnToken, "you can burn newly added token only");
    require(balanceOf(msg.sender) >= burnToken, "you can burn in your balance only");
+   require((_burnApproved1 > 0 || _burnApproved2 > 0), "you must get the approval");
+   if (_burnApproved1 > 0) {
+    require(_burnApproved1 == burnToken, "you must get the approval from approver1");
+   }
+   if (_burnApproved2 > 0) {
+    require(_burnApproved2 == burnToken, "you must get the approval from approver2");
+   }
    _burn(msg.sender, burnToken);
-   _addedSupplyToken = SafeMath.sub(_addedSupplyToken, burnToken);
+   _addedSupplyToken = (_addedSupplyToken - burnToken);
+   _burnApproved1 = 0;
+   _burnApproved2 = 0;
    return true;
  }
- function mint(uint256 addedToken) listingDT onlyOwner public returns (bool) {
-  require(SafeMath.add(_listingDate, _mint_term) <= block.timestamp, "creating new token is not yet");
+ function mint_approve_up(uint256 approveToken) onlyApprover external returns (bool) {
+  if (msg.sender == _approver1) {
+      _mintApproved1 = approveToken;
+  }
+  else if (msg.sender == _approver2) {
+      _mintApproved2 = approveToken;
+  }
+  return true;
+ }
+ function mint_approve_down() onlyApprover external returns (bool) {
+  if (msg.sender == _approver1) {
+      _mintApproved1 = 0;
+  }
+  else if (msg.sender == _approver2) {
+      _mintApproved2 = 0;
+  }
+  return true;
+ }
+ function mint(uint256 addedToken) listingDT onlyOwner external returns (bool) {
+  require((_listingDate + MINT_TERM) <= block.number, "creating new token is not yet");
+  require((_mintApproved1 > 0 || _mintApproved2 > 0), "you must get the approval");
+  if (_mintApproved1 > 0) {
+    require(_mintApproved1 == addedToken, "you must get the approval from approver1");
+   }
+   if (_mintApproved2 > 0) {
+    require(_mintApproved2 == addedToken, "you must get the approval from approver2");
+   }
   _mint(_owner, addedToken);
-  _addedSupplyToken = SafeMath.add(_addedSupplyToken, addedToken);
+  _addedSupplyToken = (_addedSupplyToken + addedToken);
+  _mintApproved1 = 0;
+  _mintApproved2 = 0;
+  return true;
+ }
+ function approve_clear() onlyOwner external returns (bool) {
+  _burnApproved1 = 0;
+  _burnApproved2 = 0;
+  _mintApproved1 = 0;
+  _mintApproved2 = 0;
   return true;
  }
 
